f3b7eeaab69185a1e52d0b58103748af4ebc17d3
gaoyang <xz@lgysh.cn>
2022-10-19 18:14:49 +0800

    优化safari收藏图标效果

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,8 @@ export default class MyDocument extends Document {
             />
 
             <link rel='manifest' href='/manifest.json' />
+            <link rel='mask-icon' href='/safari-pinned-tab.svg' />
+            <link rel="apple-touch-icon" href="/favicon-192x192.png" />
           </Head>
 
           <body>
